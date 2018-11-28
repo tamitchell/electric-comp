d13f41139c41f0bda282fde8b725723b0388ebd1
Tasha Mitchell <tamitchell14@gmail.com>
2018-11-28 12:25:39 -0500

    added reactstrap

---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../sass/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          hi
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
