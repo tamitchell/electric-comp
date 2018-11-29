109cbe8588d902eb7868897656f250df27422a0f
Tasha Mitchell <tamitchell14@gmail.com>
2018-11-29 09:01:00 -0500

    completed home-service-component

--- client/src/components/Home/Services.js
+++ client/src/components/Home/Services.js
@@ -4,14 +4,37 @@ import { Container, Row, Col, Button } from "reactstrap";
 const Services = () => {
   return (
     <Container className="home-services-container" fluid={true}>
-      <Row>
+      <Row className="row-outer">
         <h3>Top Electrical Services</h3>
-        <Col>
-        <h3>Residential</h3>
-        </Col>
-        <Col>
-        <h3>Commercial</h3>
-        </Col>
+        <Row className="row-inner">
+          <Col>
+            <h3>Residential</h3>
+            <ul>
+              <li>Aluminum Wire Inspections</li>
+              <li>Baseboard Electric Heat Installations</li>
+              <li>Ceiling Fans, Attic Fans, and Bath Exhaust Fans</li>
+              <li>Circuit Breakers</li>
+              <li>Compact Fluorescent Lamps</li>
+              <li>Electrical (Panel) Inspections</li>
+              <li>Electric Vehicle Charging Stations</li>
+              <li>Heavy-Ups and Electrical Panel Upgrades</li>
+              <li>Home Generators</li>
+            </ul>
+          </Col>
+          <Col>
+            <h3>Commercial</h3>
+            <ul>
+              <li>Build Outs</li>
+              <li>CAT5 & CAT5E Cabling</li>
+              <li>Computer & Circuits</li>
+              <li>Electrical Distribution Panel Inspections</li>
+              <li>Fluorescent Ballast Replacements</li>
+              <li>Generators</li>
+              <li>Heavy-Ups Panel Upgrades</li>
+              <li>Repair, Replacement & Installation Service</li>
+            </ul>
+          </Col>
+        </Row>
         <Button>More Services</Button>
       </Row>
     </Container>
