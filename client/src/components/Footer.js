805387017483eca18a3bea0a98e14eacf49b75f3
Tasha Mitchell <tamitchell14@gmail.com>
2018-12-24 00:30:53 -0500

    completed footer formatting

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,20 +1,36 @@
 import React from "react";
-
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row } from "reactstrap";
 
 const Footer = () => {
   return (
     <Container className="footer" fluid={true}>
       <Row className="d-none d-lg-block d-xl-block">
-        <Col>
+        <div>
           <h5 className="text-uppercase">Footer Content</h5>
           <p>
             Here you can use rows and columns here to organize your footer
             content.
           </p>
-        </Col>
-        <Col>
-          <h5 className="text-uppercase">Links</h5>
+        </div>
+        <div>
+          <h5 className="text-uppercase">Site Map</h5>
+          <ul className="list-unstyled">
+            <li>
+              <a href="#!">Home</a>
+            </li>
+            <li>
+              <a href="#!">Pricing</a>
+            </li>
+            <li>
+              <a href="#!">Services</a>
+            </li>
+            <li>
+              <a href="#!">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h5 className="text-uppercase">Our Partners</h5>
           <ul className="list-unstyled">
             <li>
               <a href="#!">Link 1</a>
@@ -29,27 +45,16 @@ const Footer = () => {
               <a href="#!">Link 4</a>
             </li>
           </ul>
-        </Col>
-        <Col>
-          <h5 className="text-uppercase">Links</h5>
-          <ul className="list-unstyled">
-            <li>
-              <a href="#!">Link 1</a>
-            </li>
-            <li>
-              <a href="#!">Link 2</a>
-            </li>
-            <li>
-              <a href="#!">Link 3</a>
-            </li>
-            <li>
-              <a href="#!">Link 4</a>
-            </li>
-          </ul>
-        </Col>
+        </div>
       </Row>
       <Row>
-          <Col>   
+        <div>
+            <p>Follow us on Social Media!</p>
+            <a href="https://twitter.com/"><img src={require("../img/social-media-svg/twitter.png")} alt="twitter" /></a>
+            <a href="https://www.instagram.com/"><img src={require("../img/social-media-svg/instagram.png")} alt="instagram" /></a>
+            <a href="https://www.facebook.com/"><img src={require("../img/social-media-svg/facebook.png")} alt="facebook" /></a>
+            <a href="https://www.linkedin.com/"><img src={require("../img/social-media-svg/linkedin-logo.png")} alt="linked in" /></a>
+        </div>
         <div className="footer-copyright text-center py-3">
           © 2018 Copyright:
           <a href="https://mdbootstrap.com/education/bootstrap/">
@@ -58,7 +63,6 @@ const Footer = () => {
           </a>
           {" "}All Rights Reserved.
         </div>
-          </Col>
       </Row>
     </Container>
   );
